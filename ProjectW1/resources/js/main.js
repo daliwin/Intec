eb6f4c1f882f24060543487eb695c879baa50812
Bryan Loyson <loysonbryan@hotmail.com>
2018-08-30 11:50:38 +0200

    pulled shark name and info from sheet and displayed them on the page

--- ProjectW1/resources/js/main.js
+++ ProjectW1/resources/js/main.js
@@ -1,23 +1,43 @@
+function renderShark(shark) {
+	title = shark.NAME
+	attack = shark.ATTACKS
+	fatalities = shark.FATALITIES
+	
+	var div = document.createElement('div')
+	var h1 = document.createElement('h1')
+	var p = document.createElement('p')
+	var br = document.createElement('br')
+	
+	div.appendChild(h1)
+	div.appendChild(p)
+	
+	h1.appendChild(document.createTextNode(title))
+	p.appendChild(document.createTextNode('Attacks: ' + attack))
+	p.appendChild(document.createTextNode('Fatalities: ' + fatalities))
+	
+	return div
+}
+
+function renderSharks(sharkData) {
+	for (var i = 0; i < sharkData.length; i++) {
+				document.getElementById('app-4').appendChild(renderShark(sharkData[i]))
+			}
+}
+
 function init() {
- var elem = document.getElementById('app-4')
- elem.style.display = 'none'
+	var elem = document.getElementById('app-4')
+	elem.style.display = 'none'
 
- Tabletop.init({
-   key: 'https://docs.google.com/spreadsheets/d/13EyQnXAngwysDCYHoEow8XwXMAaPHhkvcEMZZBxWEBs/pubhtml',
-   //key: 'https://docs.google.com/spreadsheets/d/14GTxRdrZgGe2sLrxX-KErQnWuNR2St25qgJHZXLNrhk/pubhtml',
-   callback: function (tabletopData, tabletop) {
-     console.debug(tabletopData)
-     elem.style.display = 'block'
-
-     var app4 = new Vue({
-       el: '#app-4',
-       data: {
-         list: tabletopData,
-       }
-     })
-   },
-   simpleSheet: true
- })
+	Tabletop.init({
+		key: 'https://docs.google.com/spreadsheets/d/13EyQnXAngwysDCYHoEow8XwXMAaPHhkvcEMZZBxWEBs/pubhtml',
+		callback: function (tabletopData, tabletop) {
+			console.debug(tabletopData)
+			elem.style.display = 'block'
+			
+			renderSharks(tabletopData)
+		},
+		simpleSheet: true
+	})
 }
 
 window.addEventListener('DOMContentLoaded', init)
