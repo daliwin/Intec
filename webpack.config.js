d42a45f88f093ab3717348db602b1f5f7143ea22
Bryan Loyson <loysonbryan@hotmail.com>
2018-09-06 11:59:14 +0200

    installed tabletop

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: __dirname + '/src/js/main.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
-  },
+	entry: __dirname + '/src/js/main.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'index_bundle.js'
+	},
+	externals: ['tls', 'net', 'fs'],
 
-  plugins: [
+	plugins: [
     // Generates default index.html
     new HtmlWebpackPlugin()
   ],
 
-  module: {
-    rules: [
+	module: {
+		rules: [
       // allows us to import css files
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
       },
     ]
-  },
+	},
 
-  // these settings are used by webpack-dev-server
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    historyApiFallback: true
-  }
+	// these settings are used by webpack-dev-server
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000,
+		historyApiFallback: true
+	}
 };
