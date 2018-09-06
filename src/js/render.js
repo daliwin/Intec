8afe8ec8e1c78e85268689e92ba3b941bc4ad6ae
Bryan Loyson <loysonbryan@hotmail.com>
2018-09-06 14:37:25 +0200

    added header

--- src/js/render.js
+++ src/js/render.js
@@ -1,10 +1,22 @@
 var crel = require('crel')
 
+require('../css/main.css')
+
+function renderHeader() {
+	return crel('div', {
+			class: 'hero-inner'
+		},
+		crel('h1', 'TOP 10 DEADLIEST SHARKS'))
+}
+
 function renderArticle(article) {
 	return crel('div',
 		crel('h1', article.NAME),
 		crel('p', article.ATTACKS),
-		crel('p', article.FATALITIES))
+		crel('p', article.FATALITIES),
+		crel('img', {
+			src: article.IMAGE
+		}))
 }
 
 function renderArticles(articles) {
@@ -13,8 +25,11 @@ function renderArticles(articles) {
 }
 
 function renderPage(articles) {
-	return crel('div',
-			   renderArticles(articles))
+	return crel('section', {
+			class: 'hero'
+		}, renderHeader())
+//		crel('div',
+//			renderArticles(articles)))
 }
 
 module.exports = renderPage
