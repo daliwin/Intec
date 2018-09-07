60a4160fc74c09af462d939bacfbffdaadcd0831
Bryan Loyson <loysonbryan@hotmail.com>
2018-09-07 10:28:28 +0200

    improved layout / added button

--- src/js/render.js
+++ src/js/render.js
@@ -3,46 +3,74 @@ var crel = require('crel')
 require('../css/main.css')
 
 function renderHeader() {
-    return crel('div', {
-            class: 'hero-inner'
-        }),
-        crel('h1', 'TOP 10 DEADLIEST SHARKS')
+	return crel('div', {
+			class: 'hero-inner'
+		}),
+		crel('h1', 'TOP 10 DEADLIEST SHARKS')
 }
-
 
 function renderArticle(article) {
-    return crel('div', {
-            class: 'pure-g'
-        },
-        crel('div', {class: 'pure-u-1-3'},
-            crel('h1', article.NAME),
-            crel('img', {
-                id: 'image',
-                src: article.IMAGE
-            })),
-        crel('div', {class: 'pure-u-1-3'},
-            crel('h4', 'Attacks: ', article.ATTACKS),
-            crel('h4', 'Deaths: ', article.FATALITIES)),
-        crel('div', {class: 'pure-u-1-3'},
-            crel('p', article.INFO))
-    )
+	var component = crel('div')
+	var button = crel('button', {class: 'pure-button pure-button-primary'}, 'More info')
+	
+	button.addEventListener('click', function () {
+        var content = crel('p', article.INFO)
+        component.removeChild(button)
+        component.appendChild(content)
+      })
+	
+	return crel('div', {class: 'pure-u-1 pure-u-md-1-3'},
+				component,
+			crel('h1', article.NAME),
+			crel('img', {class: 'pure-img', src: article.IMAGE}),
+			crel('h4', 'Attacks: ', article.ATTACKS),
+			crel('h4', 'Deaths: ', article.FATALITIES),
+				button)
+//			crel('p', article.INFO))
 }
+
+
+//function renderArticle(article) {
+//	return crel('div', {
+//			class: 'pure-g'
+//		},
+//		crel('div', {
+//				class: 'pure-u-1 pure-u-md-1-3'
+//			},
+//			crel('h1', article.NAME),
+//			crel('img', {
+//				class: 'pure-img',
+//				src: article.IMAGE
+//			})),
+//		crel('div', {
+//				class: 'pure-u-1 pure-u-md-1-3'
+//			},
+//			crel('h4', 'Attacks: ', article.ATTACKS),
+//			crel('h4', 'Deaths: ', article.FATALITIES)),
+//		crel('div', {
+//				class: 'pure-u-1 pure-u-md-1-3'
+//			},
+//			crel('p', article.INFO))
+//	)
+//}
 
 
 function renderArticles(articles) {
-    return articles.map(renderArticle)
+	return articles.map(renderArticle)
 }
 
 function renderPage(articles) {
-    return crel(document.body,
-        crel('section', {
-            class: 'hero'
-        }, renderHeader()),
-        //        crel('div', {
-        //                class: 'section group'
-        //            },
-        crel('div', renderArticles(articles))
-    )
+	return crel(document.body,
+		crel('section', {
+			class: 'hero'
+		}, renderHeader()),
+		//        crel('div', {
+		//                class: 'section group'
+		//            },
+		crel('div', {
+			class: 'pure-g sharks'
+		}, renderArticles(articles))
+	)
 }
 
 module.exports = renderPage
